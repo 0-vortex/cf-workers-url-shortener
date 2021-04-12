602adda671feec3aa967d3f49e8607e3feba22b1
TED Vortex <ted.vortex@gmail.com>
2021-04-12 14:00:32 +0200

    feat: added experimental landing page generation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import index from './index.handlebars';
+import links from './data.json';
+import {Color, Solver, hexToRgb} from './util';
+
 const UnexpectedError = new Error('Please provide a source');
 
 const handleRequest = async request => {
@@ -13,6 +17,38 @@ const handleRequest = async request => {
         if (redirectTo) {
           return Response.redirect(redirectTo, 301);
         }
+      } else {
+        const brands = [];
+        links.forEach(link => {
+          if (typeof link.color !== 'undefined') {
+            const rgb = hexToRgb(link.color);
+            const color = new Color(rgb[0], rgb[1], rgb[2]);
+            const solver = new Solver(color);
+            const result = solver.solve();
+
+            brands.push({
+              className: link.key,
+              filter: result.filter,
+            });
+          }
+        });
+
+        const html = index({
+          name: 'TED Vortex',
+          analytics: '123456',
+          background: 'rgba(24,23,23,1)',
+          typography: 'rgba(255,255,255,1)',
+          primary: 'rgba(184,15,10,1)',
+          secondary: 'rgba(15,82,186,1)',
+          links,
+          brands,
+        });
+
+        return new Response(html, {
+          headers: {
+            'content-type': 'text/html;charset=UTF-8',
+          },
+        });
       }
     } catch (e) {
       UnexpectedError.message = 'Key not found';
---
+++ src/util/index.js
@@ -0,0 +1,9 @@
+const Color = require('./Color');
+const Solver = require('./Solver');
+const hexToRgb = require('./hexToRgb');
+
+module.exports = {
+  Color,
+  Solver,
+  hexToRgb,
+};
